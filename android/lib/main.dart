7bcf102a86326bce2ff1b745300c8c8f3379b948
Ozan Çakmak <56032671+csOzan@users.noreply.github.com>
2022-01-07 17:14:34 +0300

    Update main.dart

--- android/lib/main.dart
+++ android/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
+} // TEST
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
