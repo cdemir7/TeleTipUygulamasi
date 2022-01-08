b94fbe685b8f80695efe3ac1e778dff1e26d8197
Ozan Çakmak <56032671+csOzan@users.noreply.github.com>
2022-01-08 10:42:12 -0800

    gitignore eklendi
    Editörümün oluşturduğu geçici dosyaların görmezden gelinmesi için gitignore dosyası oluşturdum. Ayrıca database_query dosyasına önceden yazdığım kodları ekledim

---
+++ database/database_query.sql
@@ -0,0 +1,64 @@
+Create database DB_HASTANE_TELETIP;
+
+use db_hastane_teletip;
+
+create table Hastalar
+(
+hasta_ID int auto_increment primary key,
+hasta_ISIM varchar(20) not NULL,
+hasta_SOYISIM varchar(20) not NULL,
+hasta_SIFRE varchar(20) not NULL,
+hasta_MAIL varchar(60) not null,
+hasta_FOTO varchar(100)
+);
+
+Create table Doktorlar
+(
+doktor_ID int auto_increment primary key,
+doktor_ISIM varchar(20) not NULL,
+doktor_SOYISIM varchar(20) not NULL,
+doktor_MAIL varchar(60) not NULL,
+doktor_FOTO varchar(100),
+doktor_SIFRE varchar(20) not NULL
+);
+
+Create table ABD
+(
+abd_ID int auto_increment primary key,
+abd_isim varchar(30) not NULL
+);
+Create table uzmanliklar
+(
+uzmanlik_ID int auto_increment primary key,
+uzmanlik_isim varchar(30) not NULL
+)
+Create table doktor_uzmanlik_listesi
+(
+uzmanlik_liste_ID int auto_increment primary key,
+doktor_ID int not null,
+uzmanlik_ID int not null,
+
+CONSTRAINT fk_uzmanlik_doktor 
+foreign key (doktor_ID) 
+	REFERENCES doktorlar(doktor_ID)
+		on delete cascade,
+CONSTRAINT fk_uzmanlik_uzmanlik
+foreign key (uzmanlik_ID)
+	REFERENCES uzmanliklar(uzmanlik_ID)
+        on delete cascade
+)
+Create table doktor_abd_listesi
+(
+abd_liste_ID int auto_increment primary key,
+doktor_ID int not null unique,
+abd_ID int not null,
+
+CONSTRAINT fk_abd_doktor 
+foreign key (doktor_ID) 
+	REFERENCES doktorlar(doktor_ID)
+		on delete cascade,
+CONSTRAINT fk_abd_abd
+foreign key (abd_ID)
+	REFERENCES abd(abd_ID)
+        on delete cascade
+)
