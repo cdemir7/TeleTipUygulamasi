8d5983ecc668ff30cedb680c4c519ffe4899446c
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2022-01-11 01:59:09 +0300

    Doktor ana ekranı güncellendi

--- android/lib/Screens/doktor_ana_ekrani.dart
+++ android/lib/Screens/doktor_ana_ekrani.dart
@@ -7,6 +7,13 @@ class Doktor_Ana_Ekrani extends StatefulWidget {
   _Doktor_Ana_EkraniState createState() => _Doktor_Ana_EkraniState();
 }
 
+class Doktor_Ana_Ekrani extends StatefulWidget {
+  const Doktor_Ana_Ekrani({Key? key}) : super(key: key);
+
+  @override
+  _Doktor_Ana_EkraniState createState() => _Doktor_Ana_EkraniState();
+}
+
 class Hasta {
   String ad = 'afd';
   String sikayet = 'dscsad';
@@ -14,6 +21,9 @@ class Hasta {
   Hasta(String ad, String sikayet) {
     this.ad = ad;
     this.sikayet = sikayet;
+  }
+  String toString() {
+    return '$Hasta{ad: $ad, şikayet: $sikayet}';
   }
 }
 
@@ -52,43 +62,60 @@ class _Doktor_Ana_EkraniState extends State<Doktor_Ana_Ekrani> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text(
-            'Doktor...... Hoşgeldiniz',
-            style: TextStyle(fontSize: 20),
-          ),
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Center(
+              child: Text(
+                'Doktor...... Hoşgeldiniz',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              height: 470,
+              color: Colors.greenAccent,
+              child: ListView.builder(
+                itemCount: hastalar.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Text('${hastalar[index]}'),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(children: [
+              RaisedButton(
+                highlightColor: Colors.lightBlueAccent,
+                elevation: 10.0,
+                child: Text(
+                  'Çıkış',
+                  style: TextStyle(fontSize: 13.5),
+                ),
+                onPressed: () {},
+              ),
+              SizedBox(
+                width: 180.0,
+              ),
+              RaisedButton(
+                highlightColor: Colors.lightBlueAccent,
+                elevation: 10.0,
+                child: Text(
+                  'Profile Git',
+                  style: TextStyle(fontSize: 13.5),
+                ),
+                onPressed: () {},
+              ),
+            ]),
+          ],
         ),
-        SizedBox(
-          height: 50,
-        ),
-        Container(
-          height: 470,
-          color: Colors.greenAccent,
-          child: ListView.builder(
-            itemCount: hastalar.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                child: Text('${hastalar[index]}'),
-              );
-            },
-          ),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        RaisedButton(
-          highlightColor: Colors.lightBlueAccent,
-          padding: EdgeInsets.symmetric(horizontal: 1),
-          elevation: 10.0,
-          child: Text(
-            'Çıkış',
-            style: TextStyle(fontSize: 13.5),
-          ),
-          onPressed: () {},
-        ),
-      ],
+      ),
     );
   }
 }
