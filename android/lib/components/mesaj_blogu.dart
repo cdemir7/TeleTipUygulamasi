e439d0f788ecc5b15e5a3f90bf7553d24743f892
Ozan Çakmak <56032671+csOzan@users.noreply.github.com>
2022-01-10 17:58:23 +0300

    Mesaj Baloncuğu
    Mesaj baloncuğu için taslak oluşturuldu

---
+++ android/lib/components/mesaj_blogu.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+
+class MesajBlogu extends StatelessWidget{
+
+  final Widget mesaj; // bu widgete parametre olarak bir Text widgeti gonderilcek
+
+  const MesajBlogu({
+    Key? key,
+    required this.mesaj
+  }) :super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    
+  }
+}
