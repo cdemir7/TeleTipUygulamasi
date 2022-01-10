f76843caef781f52719eda3474f94af20ace46a0
Ozan Çakmak <56032671+csOzan@users.noreply.github.com>
2022-01-10 08:35:55 +0300

    Düzenleme
    Kodda düzenleme yaptım

--- database/database_query.sql
+++ database/database_query.sql
@@ -1,7 +1,7 @@
-Create database DB_HASTANE_TELETIP;
+REATE DATABASE DB_HASTANE_TELETIP;
 
-use db_hastane_teletip;
-
+use DB_HASTANE_TELETIP;
+SET FOREIGN_KEY_CHECKS=OFF;
 create table Hastalar
 (
 hasta_ID int auto_increment primary key,
@@ -10,7 +10,7 @@ hasta_SOYISIM varchar(20) not NULL,
 hasta_SIFRE varchar(20) not NULL,
 hasta_MAIL varchar(60) not null,
 hasta_FOTO varchar(100)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 Create table Doktorlar
 (
@@ -20,33 +20,33 @@ doktor_SOYISIM varchar(20) not NULL,
 doktor_MAIL varchar(60) not NULL,
 doktor_FOTO varchar(100),
 doktor_SIFRE varchar(20) not NULL
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 Create table ABD
 (
 abd_ID int auto_increment primary key,
 abd_isim varchar(30) not NULL
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 Create table uzmanliklar
 (
 uzmanlik_ID int auto_increment primary key,
 uzmanlik_isim varchar(30) not NULL
-)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 Create table doktor_uzmanlik_listesi
 (
 uzmanlik_liste_ID int auto_increment primary key,
-doktor_ID int not null,
-uzmanlik_ID int not null,
+doktor_ID int,
+uzmanlik_ID int,
 
 CONSTRAINT fk_uzmanlik_doktor 
 foreign key (doktor_ID) 
-	REFERENCES doktorlar(doktor_ID)
-		on delete cascade,
+    REFERENCES doktorlar(doktor_ID)
+        on delete cascade,
 CONSTRAINT fk_uzmanlik_uzmanlik
 foreign key (uzmanlik_ID)
-	REFERENCES uzmanliklar(uzmanlik_ID)
+    REFERENCES uzmanliklar(uzmanlik_ID)
         on delete cascade
-)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 Create table doktor_abd_listesi
 (
 abd_liste_ID int auto_increment primary key,
@@ -55,13 +55,13 @@ abd_ID int not null,
 
 CONSTRAINT fk_abd_doktor 
 foreign key (doktor_ID) 
-	REFERENCES doktorlar(doktor_ID)
-		on delete cascade,
+    REFERENCES doktorlar(doktor_ID)
+        on delete cascade,
 CONSTRAINT fk_abd_abd
 foreign key (abd_ID)
-	REFERENCES abd(abd_ID)
+    REFERENCES abd(abd_ID)
         on delete cascade
-)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 Create table Mesajlar
 (
 mesaj_ID int auto_increment primary key,
