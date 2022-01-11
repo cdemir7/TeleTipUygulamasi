0dbd8c24d8fc1f4d52f89cd64bc9e5501903dbec
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2022-01-11 23:19:42 +0300

    Doktor ana ekranında listeleme sorunu giderildi. Cardlara tıklanabilme özelliği eklendi ve boyutları ayarlandı.

--- android/lib/Screens/doktor_ana_ekrani.dart
+++ android/lib/Screens/doktor_ana_ekrani.dart
@@ -7,28 +7,10 @@ class Doktor_Ana_Ekrani extends StatefulWidget {
   _Doktor_Ana_EkraniState createState() => _Doktor_Ana_EkraniState();
 }
 
-class Doktor_Ana_Ekrani extends StatefulWidget {
-  const Doktor_Ana_Ekrani({Key? key}) : super(key: key);
-
-  @override
-  _Doktor_Ana_EkraniState createState() => _Doktor_Ana_EkraniState();
-}
-
-class Hasta {
-  String ad = 'afd';
-  String sikayet = 'dscsad';
-
-  Hasta(String ad, String sikayet) {
-    this.ad = ad;
-    this.sikayet = sikayet;
-  }
-  String toString() {
-    return '$Hasta{ad: $ad, şikayet: $sikayet}';
-  }
-}
+String Hasta(var ad, var y) => "$ad $y";
 
 class _Doktor_Ana_EkraniState extends State<Doktor_Ana_Ekrani> {
-  List<Hasta> hastalar = [
+  List<String> hastalar = [
     Hasta('scc', 'csa'),
     Hasta('scc', 'csa'),
     Hasta('scc', 'csa'),
@@ -62,59 +44,66 @@ class _Doktor_Ana_EkraniState extends State<Doktor_Ana_Ekrani> {
   ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            Center(
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Doktor...... Hoşgeldiniz',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            height: 470,
+            child: ListView.builder(
+              itemCount: hastalar.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    child: InkWell(
+                      onTap: () async {},
+                      child: Container(
+                        child: Text('${hastalar[index]}'),
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(children: [
+            RaisedButton(
+              highlightColor: Colors.lightBlueAccent,
+              elevation: 10.0,
               child: Text(
-                'Doktor...... Hoşgeldiniz',
-                style: TextStyle(fontSize: 20),
+                'Çıkış',
+                style: TextStyle(fontSize: 13.5),
               ),
+              onPressed: () {},
             ),
             SizedBox(
-              height: 50,
+              width: 180.0,
             ),
-            Container(
-              height: 470,
-              color: Colors.greenAccent,
-              child: ListView.builder(
-                itemCount: hastalar.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    child: Text('${hastalar[index]}'),
-                  );
-                },
+            RaisedButton(
+              highlightColor: Colors.lightBlueAccent,
+              elevation: 10.0,
+              child: Text(
+                'Profile Git',
+                style: TextStyle(fontSize: 13.5),
               ),
+              onPressed: () {},
             ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(children: [
-              RaisedButton(
-                highlightColor: Colors.lightBlueAccent,
-                elevation: 10.0,
-                child: Text(
-                  'Çıkış',
-                  style: TextStyle(fontSize: 13.5),
-                ),
-                onPressed: () {},
-              ),
-              SizedBox(
-                width: 180.0,
-              ),
-              RaisedButton(
-                highlightColor: Colors.lightBlueAccent,
-                elevation: 10.0,
-                child: Text(
-                  'Profile Git',
-                  style: TextStyle(fontSize: 13.5),
-                ),
-                onPressed: () {},
-              ),
-            ]),
-          ],
-        ),
+          ]),
+        ],
       ),
     );
   }
