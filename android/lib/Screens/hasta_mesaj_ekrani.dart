a032d4bdcd857bd1a0d1e5060c718583e490d5c4
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2022-01-11 23:31:50 +0300

    Hasta genel mesaj ekranında listeleme sorunu giderildi. Cardlara tıklanabilme özelliği eklendi ve boyutları ayarlandı.

--- android/lib/Screens/hasta_mesaj_ekrani.dart
+++ android/lib/Screens/hasta_mesaj_ekrani.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(Hasta_Mesaj_Ekrani());
+
 class Hasta_Mesaj_Ekrani extends StatefulWidget {
   const Hasta_Mesaj_Ekrani({Key? key}) : super(key: key);
 
@@ -7,22 +9,11 @@ class Hasta_Mesaj_Ekrani extends StatefulWidget {
   _Hasta_Mesaj_EkraniState createState() => _Hasta_Mesaj_EkraniState();
 }
 
-class Hasta {
-  String ad = 'afd';
-  String sikayet = 'dscsad';
-
-  Hasta(String ad, String sikayet) {
-    this.ad = ad;
-    this.sikayet = sikayet;
-  }
-  String toString() {
-    return '${Hasta} {ad: $ad, sikayet: $sikayet}';
-  }
-}
+String Hasta(var ad, var y) => "$ad $y";
 
 class _Hasta_Mesaj_EkraniState extends State<Hasta_Mesaj_Ekrani> {
   @override
-  List<Hasta> konusmalarim = [
+  List<String> konusmalarim = [
     Hasta('scc', 'csa'),
     Hasta('scc', 'csa'),
     Hasta('scc', 'csa'),
@@ -56,33 +47,48 @@ class _Hasta_Mesaj_EkraniState extends State<Hasta_Mesaj_Ekrani> {
   ];
 
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text(
-            'Konuşmalarım',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        Container(
-          height: 670,
-          child: ListView.builder(
-            itemCount: konusmalarim.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                color: Colors.lightBlueAccent,
+    return Scaffold(
+      body: Wrap(
+        children: [
+          Column(
+            children: [
+              Center(
                 child: Text(
-                  '${konusmalarim[index]}',
-                  style: TextStyle(color: Colors.white),
+                  'Konuşmalarım',
+                  style: TextStyle(fontSize: 20),
                 ),
-              );
-            },
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 670,
+                child: ListView.builder(
+                  itemCount: konusmalarim.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Card(
+                        child: InkWell(
+                          onTap: () async {},
+                          child: Container(
+                            child: Text(
+                              '${konusmalarim[index]}',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            width: 100.0,
+                            height: 100.0,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
