0124707322188c5cbf9e261030e05f06ac153957
Ozan Çakmak <56032671+csOzan@users.noreply.github.com>
2022-01-13 05:13:06 +0300

    Yenı Fonksıyonlar Yazdım

--- android/lib/server_util/processed_requests.dart
+++ android/lib/server_util/processed_requests.dart
@@ -1,9 +1,10 @@
 import 'package:android/server_util/classes.dart';
+import 'package:android/server_util/requests.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 
-Future doktorEkraniSorgusu(String doktor_MAIL, String doktor_SIFRE) async {
+Future doktorGirisSorgusu(String doktor_MAIL, String doktor_SIFRE) async {
     //replace your restFull API here.
     var url = "http://37.75.8.238:3000/api/doktorlar/";
     final response = await http.get(Uri.parse(url));
@@ -32,7 +33,7 @@ Future doktorEkraniSorgusu(String doktor_MAIL, String doktor_SIFRE) async {
   }
   return "OKUMUYOR";
 }
-Future<List<Mesaj>> mesajEkraniSorgusu(int doktor_Id, int hasta_ID) async {
+Future<List<Mesaj>> mesajEkraniSorgusu(int doktor_ID, int hasta_ID) async {
     //replace your restFull API here.
     var url = "http://37.75.8.238:3000/api/mesajlar/";
     final response = await http.get(Uri.parse(url));
@@ -52,7 +53,7 @@ Future<List<Mesaj>> mesajEkraniSorgusu(int doktor_Id, int hasta_ID) async {
           gonderen: singleMesaj['gonderen']
           );
       //Adding user to the list.
-      if(doktor_Id == mesaj.doktor_ID && hasta_ID == mesaj.hasta_ID){
+      if(doktor_ID == mesaj.doktor_ID && hasta_ID == mesaj.hasta_ID){
         mesajlar.add(mesaj);
       }
       
@@ -61,7 +62,7 @@ Future<List<Mesaj>> mesajEkraniSorgusu(int doktor_Id, int hasta_ID) async {
     return mesajlar;
 }
 Future<List<Abd>> abdDoktor(String abd_START) async {
-    //  abd_START ile baslayan anabilim dalina mensup doktorlari serverdan ceker
+    //replace your restFull API here.
     var url = "http://37.75.8.238:3000/api/doktorlar/anabilimdali";
     final response = await http.get(Uri.parse(url));
 
@@ -83,16 +84,170 @@ Future<List<Abd>> abdDoktor(String abd_START) async {
     }
     return abdler;
 }
-mesajGonder(String mesaj, int doktor_ID, int hasta_ID, String gonderen){}
-essizMesajListesi(int doktor_ID, int hasta_ID){}
-doktorAyarGorunumu(int doktor_ID){}
+Future <int> hastaKayit(String hasta_ISIM, String hasta_SOYISIM, String hasta_MAIL, String hasta_SIFRE)async{
+  List<Hasta> hastaList = await hastaGetRequest();
+  for (var item in hastaList) {
+    if(item.hasta_MAIL == hasta_MAIL){
+      return 2;
+    }
+  }
+  await hastaGonder( hasta_ISIM,  hasta_SOYISIM,  hasta_MAIL,  hasta_SIFRE);
+  return 0;
+}
+Future<http.Response> hastaGonder(String hasta_ISIM, String hasta_SOYISIM, String hasta_MAIL, String hasta_SIFRE) async{
+  return await http.post(
+    // servere hasta verisi gondermek icin kullanilir
+    // kendi basina kullanma, hastaKayit fonksiyonu ile kullan
+    Uri.parse("http://37.75.8.238:3000/api/hastalar"), 
+    headers: <String, String>{'Content-Type':'application/json; charset=UTF-8',
+  }, body: jsonEncode(<String,dynamic>{
+    'hasta_ISIM':hasta_ISIM,
+    'hasta_SOYISIM':hasta_SOYISIM,
+    'hasta_SIFRE':hasta_SIFRE,
+    'hasta_MAIL':hasta_MAIL}));
+}
+Future<http.Response> mesajGonder(int doktor_ID, int hasta_ID, String mesaj, String gonderen) async{
+  return await http.post(
+    Uri.parse("http://37.75.8.238:3000/api/mesajlar"), 
+    headers: <String, String>{'Content-Type':'application/json; charset=UTF-8',
+  },body: jsonEncode(<String,dynamic>{
+    'doktor_ID': doktor_ID, 
+    'hasta_ID':hasta_ID,
+    'mesaj':mesaj,
+    'gonderen':gonderen}));
+}
+
 doktorAyarDegisim(int dokot_ID, String doktor_ISIM, String doktor_SOYISIM, String doktor_SIFRE){}
-// doktoru cagir, olan veri icindeki ile post yap
-hastaEkraniSorgusu(String hasta_MAIL, String hasta_SIFRE){}
-hastaKayit(String hasta_ISIM, String hasta_SOYAD, String hasta_MAIL, String hasta_SIFRE){}
-isimDoktor(String doktor_ISIM){}
-uzmanlikDoktor(String uzmanlik_ISIM){}
-hastaAyarGorunumu(int hasta_ID){}
+
+isimDoktor(String doktor_START)async{
+  // isime gore doktor arar
+  List<Doktor> eskiDoktorList = await doktorGetRequest();
+  List<Doktor> yeniDoktorList = [];
+
+  for (var item in eskiDoktorList) {
+    if((item.doktor_ISIM+" "+item.doktor_SOYISIM).startsWith(doktor_START)){
+      yeniDoktorList.add(item);
+    }
+  }
+  return yeniDoktorList;
+}
+
+uzmanlikDoktor(String uzmanlik_START) async{
+  List <Uzmanlik> eskiList = await uzmanlikGetRequest();
+  List <Uzmanlik> yeniList = [];
+  for (var item in eskiList) {
+    if((item.uzmanlik_ISIM).startsWith(uzmanlik_START)){
+      yeniList.add(item);
+    }
+    return yeniList;
+  }
+}
+hastaGirisSorgusu(String hasta_MAIL, String hasta_SIFRE){}
 hastaAyarDegisimi(int hasta_ID, String hasta_ISIM, String hasta_SOYISIM, String hasta_SIFRE){}
+
+
+
+
+
+
+/* Future <Doktor> doktorAyarGorunumu(int doktor_ID)async{
+  var url = "http://37.75.8.238:3000/api/doktorlar";
+  final response = await http.get(Uri.parse(url));
+
+  var responseData = json.decode(response.body);
+
+  List<Doktor> doktorlar = [];
+  for (var singleDoktor in responseData) {
+    Doktor doktor = Doktor(
+    doktor_SIFRE: singleDoktor["doktor_SIFRE"],
+    doktor_ID: singleDoktor["doktor_ID"],
+    doktor_ISIM: singleDoktor["doktor_ISIM"],
+    doktor_SOYISIM: singleDoktor["doktor_SOYISIM"],
+    doktor_MAIL: singleDoktor["doktor_MAIL"]
+    );
+    doktorlar.add(doktor);
+  }
+  for (var item in doktorlar) {
+    if(item.doktor_ID == doktor_ID){
+      return item;
+    }
+  }
+  return null;
+} */
+
+hastaAyarGorunumu(int hasta_ID){}
+
+//mesajGonder(String mesaj, int doktor_ID, int hasta_ID, String gonderen){}
+essizMesajListesi(int? doktor_ID, int? hasta_ID, String kimden) async{// buglu
+  
+  var url = "http://37.75.8.238:3000/api/mesajlar/";
+  final response = await http.get(Uri.parse(url));
+
+  var responseData = json.decode(response.body);
+
+    //Creating a list to store input data;
+  List<Mesaj> mesajlar = [];
+  
+  if(kimden.toLowerCase() == 'doktor'){
+    
+    for (var singleMesaj in responseData) {
+      Mesaj mesaj = Mesaj(
+          mesaj_ID: singleMesaj['mesaj_ID'],
+          doktor_ID: singleMesaj["doktor_ID"],
+          hasta_ID: singleMesaj['hasta_ID'],
+          mesaj: singleMesaj['mesaj'],
+          eklenti_path: singleMesaj['eklenti_path'],
+          mesaj_tarihi: singleMesaj['mesaj_tarihi'],
+          gonderen: singleMesaj['gonderen']
+          );
+      //Adding user to the list.
+      if(doktor_ID == mesaj.doktor_ID){
+        if (essizmi(mesajlar, mesaj, kimden)) {
+          mesajlar.add(mesaj);
+        }
+      }
+      
+    }
+  } else if (kimden.toLowerCase() == 'hasta'){
+    
+    for (var singleMesaj in responseData) {
+      Mesaj mesaj = Mesaj(
+          mesaj_ID: singleMesaj['mesaj_ID'],
+          doktor_ID: singleMesaj["doktor_ID"],
+          hasta_ID: singleMesaj['hasta_ID'],
+          mesaj: singleMesaj['mesaj'],
+          eklenti_path: singleMesaj['eklenti_path'],
+          mesaj_tarihi: singleMesaj['mesaj_tarihi'],
+          gonderen: singleMesaj['gonderen']
+          );
+      //Adding user to the list.
+      if(hasta_ID == mesaj.hasta_ID){
+        if (essizmi(mesajlar, mesaj, kimden)) {
+          mesajlar.add(mesaj);
+        }
+      }
+    }
+  }
+  return mesajlar;
+}
+
+bool essizmi(List<Mesaj> liste, Mesaj mesaj, String kimden){
+  if(kimden.toLowerCase() == 'doktor'){ // doktorun listesinde ayni hastandan varmi diye kontrol edilir
+    for (var item in liste) {
+      if (mesaj.hasta_ID == item.hasta_ID) {
+        return false;
+      }
+    }
+    return true;
+  }
+  else{ // hastanin listesinde ayni doktordan varmi diye kontrol edilir
+    for (var item in liste) {
+      if (mesaj.doktor_ID == item.doktor_ID) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
 
 
